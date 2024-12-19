ab7d34e7adeb2a8b53c8f30a8f187f90dd11cfe9
Nithinkumar-05 <nithineruventi@outlook.com>
2024-12-19 11:52:19 +0530

    first commit

---
+++ FrontEnd/screens/HomeScreen.js
@@ -0,0 +1,7 @@
+export default function HomeScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Welcome to the Home Screen!</Text>
+        </View>
+    );
+}
